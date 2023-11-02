943d0d0cad50259136251bd346d0a4ab59c37fc4
Farel Winalda <135517117+FarelW@users.noreply.github.com>
2023-11-03 00:47:17 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <div className=' flex flex-col justify-center items-center w-screen h-screen'>
+      <img src="kodoksad.ico" alt="halo"/>
+      <h1 className="text-3xl text-center font-bold">
+        IVAN KONTOL 123 !!
+      </h1>
+    </div>
+  )
+}
+
+export default App;
