32d1d71a97edf62c45210599187ad989721d1af2
ChaiGans <elbertchailes888@gmail.com>
2023-11-05 18:48:13 +0700

    add: card for displaying result

---
+++ src/components/ResultCard.js
@@ -0,0 +1,12 @@
+const ResultCard = (props) => {
+    return (
+      <div className="border-white max-w-[40%] border-2 flex flex-col">
+        <div className="border-white w-full order-2 h-[120px] border-b-2"> 
+        <img src={props.imageResult} className="w-full h-full" alt="result"></img>
+        </div>
+        <div className="border-white bg-black bg-opacity-60 w-full order-2 h-[30px] flex justify-center items-center font-aenoniklight text-[12px]">Similarity : {props.similarity}%</div>
+      </div>
+    );
+}
+
+export default ResultCard;
