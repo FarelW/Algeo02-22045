36db8664dc6a99bf738ecf84249a41da20d110ab
Bodleh <tfprince079@gmail.com>
2023-11-09 00:10:09 +0700

    chore: add image and update Technology.js

--- src/pages/Technology.js
+++ src/pages/Technology.js
@@ -7,9 +7,29 @@ function Technology() {
                 <img
                     src="backgroundCamera.jpg"
                     alt="Background"
-                    className="fixed inset-0 object-cover w-full h-full"
+                    className="fixed inset-0 object-cover w-full h-full "
                 />
                 <div className="bg-black bg-opacity-60 absolute inset-0"></div>
+                <div className="grid grid-cols-2 mt-48 lg:mx-48 gap-8 ">
+                    <div className="relative text-white text-3xl font-aenonikbold text-center">
+                        Front-end
+                        <div className="flex">
+                            <img 
+                                src="tailwind.png"
+                                alt="tailwind logo"    
+                                className="w-3/6 h-3/6"                  
+                            />
+                            <img 
+                                src="react.png"
+                                alt="react logo"
+                                className="w-5/12 h-5/12"
+                            />
+                        </div>
+                    </div>
+                    <p className="relative text-white text-3xl font-aenonikbold text-center">
+                        Back-end
+                    </p>
+                </div>
             </main>
         </div>
     );
