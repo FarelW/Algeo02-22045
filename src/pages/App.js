63c608b2c29093d24deef853f846e3b302ab753b
Bodleh <tfprince079@gmail.com>
2023-11-08 13:51:19 +0700

    add technology page and fixing dev

--- src/pages/App.js
+++ src/pages/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "../components/Header";
 import Home from "./Home";
 import Developers from "./Developers";
+import Technology from "./Technology";
 import Application from "./Application";
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/guides" element={<Home />} />
-          <Route path="/technology" element={<Home />} />
+          <Route path="/technology" element={<Technology />} />
           <Route path="/developers" element={<Developers />} />
           <Route path="/application" element={<Application />} />
         </Routes>
