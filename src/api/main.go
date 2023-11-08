a476886feb7d83296a6da7ce7d203b35b4b26fff
Farel Winalda <eeksapi321@gmail.com>
2023-11-08 22:03:27 +0700

    feat : read zip file from user and return it to frontend as base64 from backend

--- src/api/main.go
+++ src/api/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"os"
+	// "fmt"
+	// "os"
+	"archive/zip"
+	"bytes"
+	"encoding/base64"
+	// "io"
 	"log"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
@@ -19,97 +23,94 @@ type Uploaded struct {
 }
 
 func main ()  {
-	app := fiber.New()
-	bios:=[]Uploaded{}
+	// app := fiber.New()
 
-	app.Static("/uploads", "./uploads")
+	app := fiber.New(fiber.Config{
+		BodyLimit: 2 * 1024 * 1024 * 1024, 
+	})
 
 	app.Use(cors.New(cors.Config{
-		AllowOrigins: "http://localhost:3000",
-		AllowHeaders: "Origin,Content-Type,Accept",
+		AllowOrigins: "*",
+		AllowHeaders: "Origin, Content-Type, Accept",
 	}))
 
-	app.Get("/healthcheck", func(c *fiber.Ctx)error{
+	app.Get("/healthcheck", func(c *fiber.Ctx) error {
 		return c.SendString("OK")
 	})
 
 	app.Post("/api/upload", func(c *fiber.Ctx) error {
-
-        fileHeader, err := c.FormFile("file")
-        if err != nil {
-            log.Printf("Error retrieving the file: %v\n", err)
-            return err
-        }
-
-        // Open the file
-        file, err := fileHeader.Open()
-        if err != nil {
-            log.Printf("Error opening the file: %v\n", err)
-            return err
-        }
-        defer file.Close()
-
-		uploadDir := "./uploads"
-
-		// Empty the directory before saving the new file
-		dir, err := os.Open(uploadDir)
-		if err != nil {
-			log.Printf("Error opening the directory: %v\n", err)
-			return err
-		}
-		defer dir.Close()
+		log.Println("Upload endpoint hit")
 	
-		files, err := dir.Readdirnames(-1)
+		response := make(map[string]string)
+	
+		// Handle Image File Upload
+		imageFileHeader, err := c.FormFile("imageFile")
 		if err != nil {
-			log.Printf("Error reading the directory: %v\n", err)
-			return err
+			log.Printf("Error retrieving the image file: %v\n", err)
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "image file not found"})
 		}
 	
-		for _, d := range files {
-			err := os.RemoveAll(filepath.Join(uploadDir, d))
-			if err != nil {
-				log.Printf("Error removing file: %v\n", err)
-				return err
+		imageFile, err := imageFileHeader.Open()
+		if err != nil {
+			log.Printf("Error opening the image file: %v\n", err)
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "error opening image file"})
+		}
+		defer imageFile.Close()
+	
+		buf := new(bytes.Buffer)
+		buf.ReadFrom(imageFile)
+		response["imageFile"] = base64.StdEncoding.EncodeToString(buf.Bytes())
+		log.Println("Image file processed")
+	
+		// Handle ZIP File Upload
+		zipFileHeader, err := c.FormFile("zipFile")
+		if err != nil {
+			log.Printf("Error retrieving the ZIP file: %v\n", err)
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "zip file not found"})
+		}
+	
+		zipFile, err := zipFileHeader.Open()
+		if err != nil {
+			log.Printf("Error opening the ZIP file: %v\n", err)
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "error opening zip file"})
+		}
+		defer zipFile.Close()
+	
+		zipReader, err := zip.NewReader(zipFile, zipFileHeader.Size)
+		if err != nil {
+			log.Printf("Error reading the ZIP file: %v\n", err)
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "error reading zip file"})
+		}
+	
+		for _, file := range zipReader.File {
+			if filepath.Ext(file.Name) == ".png" || filepath.Ext(file.Name) == ".jpg" {
+				// log.Printf("Opening %s", file.Name)
+				f, err := file.Open()
+				if err != nil {
+					log.Printf("Error opening file inside the ZIP: %s, %v\n", file.Name, err)
+					continue
+				}
+	
+				buf := new(bytes.Buffer)
+				buf.ReadFrom(f)
+				f.Close()
+	
+				response[file.Name] = base64.StdEncoding.EncodeToString(buf.Bytes())
+				// log.Printf("Processed file from ZIP: %s\n", file.Name)
 			}
 		}
-
-		imagePathUp := fmt.Sprintf("/uploads/%s", fileHeader.Filename)
-
-		filePath := fmt.Sprintf("./uploads/%s", fileHeader.Filename)
-		if err := c.SaveFile(fileHeader, filePath); err != nil {
-			log.Printf("Error saving file: %v\n", err)
-			return err
-		}
-
-		var tes texture.Vector
-
- 		// Pass the file to the ProcessFile function
-		tes  = texture.TextureProcessing(filePath)
-		//  if err != nil {
-		// 	 log.Printf("Error processing the file: %v\n", err)
-		// 	 return err
-		//  }		
-
-        bio := Uploaded{
-            FileName:      fileHeader.Filename,
-            TextureValue: tes,
-			ImagePath: imagePathUp,
-        }
-
-        // Append the new Biodata entry to bios
-		if len(bios)==0 {
-			bios = append(bios, bio)	
-		} else {
-			bios[0] = bio
-		}
-
-        // Return the updated bios
-        return c.JSON(bios)
-
+	
+		log.Println("Sending response")
+		return c.JSON(response)
 	})
+	
 
-	app.Get("/api/upload", func(c *fiber.Ctx)error{
-		return c.JSON(bios)
+	app.Get("/api/upload", func(c *fiber.Ctx) error {
+		data := map[string]interface{}{
+			"message": "GET request to /api/upload handled successfully",
+		}
+	
+		return c.JSON(data)
 	})
 
 	if err := app.Listen(":8080"); err != nil {
