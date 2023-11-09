948c39fe10dd41bf5264eedeb0e386fa5e70b4f9
ChaiGans <elbertchailes888@gmail.com>
2023-11-10 03:24:06 +0700

    feat: add dropdown integrated and form

--- src/pages/Application.js
+++ src/pages/Application.js
@@ -1,60 +1,125 @@
 import React from "react";
 import Toast from "../components/Toast";
 import ResultCard from "../components/ResultCard";
+import FolderDropzone from "../components/FolderDropzone";
 
 const Application = () => {
     // data yang bisa diambil adalah imageFile dan dataset untuk dipass ke backend
-    const [image, setImage] = React.useState(null);
-    const [imageFile, setImageFile] = React.useState(null); 
-    const [dataset, setDataset] = React.useState(null);
-    const [fileName, setFileName] = React.useState(null);
-    // isOn===true berarti pemrosesan dilakukan dengan metode Colors, sedangkan isOn===false pemrosesan dilakukan dengan metode Texture
-      const [isOn, setIsOn] = React.useState(true);
+    const [image, setImage] = React.useState(null); //base64-encoded string
+    const [selectedFiles, setSelectedFiles] = React.useState([]); // array of file
+    const [fileName, setFileName] = React.useState(null); // just to print filename, igonre it for backend
+    // isColor===true berarti pemrosesan dilakukan dengan metode Colors, sedangkan isColor===false pemrosesan dilakukan dengan metode Texture
+    const [isColor, setisColor] = React.useState(true);
 
-      const toggleSwitch = () => setIsOn(!isOn);
+      const toggleSwitch = () => setisColor(!isColor);
+
+      const handleFilesAdded = (newFiles) => {
+        setSelectedFiles(newFiles);
+      };
+  const handleSubmit = (event) => {
+
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append("proccesstype", isColor);
+    formData.append("image", image); // Append the base64 image data
+    for (let i = 0; i < selectedFiles.length; i++) {
+      formData.append("selectedFiles[]", selectedFiles[i]);
+    }
+
+    // Pake tiga ini buat di post ke backend ya ler
+    console.log(formData.get("proccesstype")); // Log the "proccesstype" value (true maka color processing, false maka texture)
+    console.log(formData.get("image")); // Log the "image" value (base-64 string)
+    console.log(formData.getAll("selectedFiles[]")); // Log array of selectedFiles
+
+    // // Perform the POST request to the server here using fetch or an HTTP client library
+    // // Example using fetch:
+    // fetch("your-server-endpoint", {
+    //   method: "POST",
+    //   body: formData,
+    // })
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     // Handle the server response here
+    //     console.log("Server response:", data);
+    //   })
+    //   .catch((error) => {
+    //     // Handle any errors that occur during the POST request
+    //     console.error("Error:", error);
+    //   });
+  };
 
     function handleFileChange(event) {
-        const file = event.target.files[0];
-        if (file) {
+      const file = event.target.files[0];
+      if (file) {
         const reader = new FileReader();
         reader.onload = (e) => {
-            setImage(e.target.result);
-            setFileName(file.name);
-            setImageFile(file);
+          // Update the state immediately when the file is loaded
+          setImage(e.target.result);
+          setFileName(file.name);
         };
         reader.readAsDataURL(file);
-        }
-    }
+      }
+}
+
+   
 
   const [toast, setToast] = React.useState({ message: "", type: "" });
 
-  const handleZipChange = (event) => {
-    const file = event.target.files[0];
-    if (file) {
-      const fileExtension = file.name.split(".").pop().toLowerCase();
-      if (fileExtension === "zip") {
-        setDataset(file);
-        setToast({ message: "Dataset uploaded successfully!", type: "success" });
-      } else {
-        setToast({ message: "Only ZIP files are allowed!", type: "error" });
+  const showToast = (message, type) => {
+    setToast({ message, type });
+  };
+
+  const handleMultipleUploadChange = (event) => {
+    const files = event.target.files;
+    if (files.length > 0) {
+      const validImageExtensions = ["jpg", "jpeg", "png"];
+      const selectedImages = [];
+
+      for (let i = 0; i < files.length; i++) {
+        const file = files[i];
+        const fileExtension = file.name.split(".").pop().toLowerCase();
+
+        if (validImageExtensions.includes(fileExtension)) {
+          selectedImages.push(file);
+        } else {
+          // You can handle invalid file types here if needed.
+          console.log(`Invalid file: ${file.name}`);
+        }
+      }
+      
+      setSelectedFiles(selectedImages);
+
+      if (selectedImages.length > 0) {
+        // Handle successful image upload here if needed.
+        console.log(`Uploaded ${selectedImages.length} images.`);
       }
     }
   };
-React.useEffect(() => {
-  let timeout;
 
-  if (toast.message) {
-    timeout = setTimeout(() => {
-      setToast({ message: "", type: "" });
-    }, 3000);
-  }
 
-  return () => {
-    if (timeout) {
-      clearTimeout(timeout);
-    }
-  };
-}, [toast.message]);
+    React.useEffect(() => {
+      let timeout;
+
+      if (toast.message) {
+        timeout = setTimeout(() => {
+          setToast({ message: "", type: "" });
+        }, 3000);
+      }
+
+      return () => {
+        if (timeout) {
+          clearTimeout(timeout);
+        }
+      };
+    }, [toast.message]);
+
+    React.useEffect(() => {
+      if (selectedFiles.length !== 0) {
+        showToast(`Uploaded ${selectedFiles.length} images.`, "success");
+      }
+    }, [selectedFiles]);
+
+
     return (
       <div className="min-w-screen min-h-screen flex flex-col">
         <main className="flex-1 relative">
@@ -66,88 +131,141 @@ React.useEffect(() => {
           <div className="bg-black bg-opacity-60 absolute inset-0"></div>
           <div className="relative z-[2] pt-32 flex items-start text-white flex-row px-6">
             {/* leftside */}
-            <div className="w-[50%] px-8 gap-y-4 flex flex-col">
-              <button
-                className="w-fit place-self-center flex items-center gap-x-3 border-2 px-4 py-1 rounded-md bg-white text-black hover:cursor-pointer hover:bg-slate-600 hover:border-black hover:text-white transition-all duration-300"
-                onClick={() => document.getElementById("getFile").click()}
-              >
-                Insert an Image
-              </button>
-              <input
-                type="file"
-                id="getFile"
-                onChange={handleFileChange}
-                className="hidden"
-                accept="image/*"
-              />
-              {image ? (
-                <p className="text-white font-aenoniklight text-[15px] text-center">
-                  {fileName}
-                </p>
-              ) : (
-                <p className="text-white font-aenoniklight text-[15px] text-center">
-                  No file read.
-                </p>
-              )}
-              <div
-                className={`relative w-auto max-h-[360px] ${
-                  image ? "" : "min-h-[360px]"
-                } border-white border`}
-              >
-                {image && (
-                  <img
-                    src={image}
-                    alt="Uploaded"
-                    className="max-h-[360px] mx-auto"
-                  />
-                )}
-                <div className="absolute right-4 bottom-4">
-                  <div
-                    className={`w-20 h-8 flex items-center border-white border bg-gray-300 rounded-full p-1 cursor-pointer ${
-                      isOn ? "bg-red-600" : "bg-yellow-500"
-                    }`}
-                    onClick={toggleSwitch}
+            <div className="w-[50%] px-8 flex flex-col">
+              <form onSubmit={handleSubmit} className="space-y-4">
+                <div className="flex flex-row items-center justify-center gap-x-5">
+                  <button type="button"
+                    className="w-fit place-self-center flex items-center gap-x-3 border-2 px-4 py-1 rounded-md bg-white text-black hover:cursor-pointer hover:bg-slate-600 hover:border-black hover:text-white transition-all duration-300"
+                    onClick={() => document.getElementById("getFile").click()}
                   >
-                    {isOn ? (
-                      <p className="absolute text-white left-4 font-aenoniklight text-[12px]">
-                        Color
-                      </p>
-                    ) : (
-                      <p className="absolute text-black right-2 font-aenoniklight text-[12px]">
-                        Texture
-                      </p>
-                    )}
+                    Insert an Image
+                  </button>
+                  <input
+                    type="file"
+                    id="getFile"
+                    onChange={handleFileChange}
+                    className="hidden"
+                    accept="image/*"
+                  />
+                  {image ? (
+                    <p className="text-white font-aenoniklight text-[15px] text-center">
+                      {fileName}
+                    </p>
+                  ) : (
+                    <p className="text-white font-aenoniklight text-[15px] text-center">
+                      No file read.
+                    </p>
+                  )}
+                </div>
 
+                <div
+                  className={`relative w-auto max-h-[300px] ${
+                    image ? "" : "min-h-[300px]"
+                  } border-white border`}
+                >
+                  {image && (
+                    <img
+                      src={image}
+                      alt="Uploaded"
+                      className="max-h-[298px] mx-auto"
+                    />
+                  )}
+                  <div className="absolute right-4 bottom-4">
                     <div
-                      className={`bg-black w-6 h-6 rounded-full shadow-md transform duration-300 ease-in-out ${
-                        isOn ? "translate-x-12" : ""
+                      className={`w-20 h-8 flex items-center border-white border bg-gray-300 rounded-full p-1 cursor-pointer ${
+                        isColor ? "bg-red-600" : "bg-yellow-500"
                       }`}
-                    ></div>
+                      onClick={toggleSwitch}
+                    >
+                      {isColor ? (
+                        <p className="absolute text-white left-4 font-aenoniklight text-[12px]">
+                          Color
+                        </p>
+                      ) : (
+                        <p className="absolute text-black right-2 font-aenoniklight text-[12px]">
+                          Texture
+                        </p>
+                      )}
+
+                      <div
+                        className={`bg-black w-6 h-6 rounded-full shadow-md transform duration-300 ease-in-out ${
+                          isColor ? "translate-x-12" : ""
+                        }`}
+                      ></div>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="flex flex-row space-x-7 justify-center">
-                <button className="w-fit place-self-center flex items-center gap-x-3 border-2 px-4 py-1 rounded-md bg-white text-black hover:cursor-pointer hover:bg-slate-600 hover:border-black hover:text-white transition-all duration-300">
-                  Search
-                </button>
-                <button
-                  onClick={() => document.getElementById("getDataset").click()}
-                  className="w-fit place-self-center flex items-center gap-x-3 border-2 px-4 py-1 rounded-md bg-white text-black hover:cursor-pointer hover:bg-slate-600 hover:border-black hover:text-white transition-all duration-300"
-                >
-                  Upload Dataset
-                </button>
-                <input
-                  type="file"
-                  id="getDataset"
-                  className="hidden"
-                  accept=".zip"
-                  onChange={handleZipChange}
-                ></input>
-                {/* dataset bakal dipass ke backend */}
-                {toast.message && (
-                  <Toast message={toast.message} type={toast.type} />
-                )}
-              </div>
+                <div className="flex flex-col space-y-4 items-center">
+                  <div className="flex flex-row justify-center items-center space-x-4">
+                    <button
+                      type="submit"
+                      className="w-fit place-self-center flex items-center gap-x-3 border-2 px-4 py-1 rounded-md bg-white text-black hover:cursor-pointer hover:bg-slate-600 hover:border-black hover:text-white transition-all duration-300"
+                    >
+                      Search
+                    </button>
+                    <div className="relative">
+                      <button type="button"
+                        onClick={() =>
+                          document.getElementById("getDataset").click()
+                        }
+                        className="w-fit place-self-center flex items-center gap-x-3 border-2 px-4 py-1 rounded-md bg-white text-black hover:cursor-pointer hover:bg-slate-600 hover:border-black hover:text-white transition-all duration-300"
+                      >
+                        Upload Dataset
+                      </button>
+                      {/* {dataset ? (
+                      <button
+                        onClick={() => {
+                          setDataSetFilename(null);
+                          setDataset(null);
+                        }}
+                        className="w-5 h-5 rounded-full flex justify-center items-center bg-red-800 absolute right-[-7px] top-[-7px] font-aenonikbold"
+                      >
+                        X
+                      </button>
+                    ) : (
+                      ""
+                    )} */}
+                    </div>
+                    {/* {dataset ? (
+                    <p className="text-white font-aenoniklight">
+                      {dataSetFilename}
+                    </p>
+                  ) : (
+                    <p className="text-white font-aenoniklight">
+                      No dataset uploaded.
+                    </p>
+                  )} */}
+
+                    <input
+                      type="file"
+                      id="getDataset"
+                      className="hidden"
+                      accept="image/*"
+                      multiple
+                      onChange={handleMultipleUploadChange}
+                    ></input>
+                    {/* dataset bakal dipass ke backend */}
+                    {toast.message && (
+                      <Toast message={toast.message} type={toast.type} />
+                    )}
+                    <button type="button"
+                      className=" text-white bg-red-500 border-2 font-aenonikbold px-3 border-black py-1 rounded-lg"
+                      onClick={() => {
+                        setSelectedFiles([]);
+                        showToast("Dropzone has been cleared.", "errors");
+                      }}
+                    >
+                      CLEAR ALL
+                    </button>
+                  </div>
+                  <div className="w-full h-[80px]">
+                    <FolderDropzone
+                      currentState={selectedFiles}
+                      onFilesAdded={handleFilesAdded}
+                    />
+                  </div>
+                </div>
+              </form>
             </div>
 
             {/* rightside */}
@@ -155,15 +273,25 @@ React.useEffect(() => {
               <div className="flex flex-1 flex-col justify-center h-[500px] items-center font-aenonikregular text-[18px] gap-y-4">
                 <p className="text-white">54 Results in 0.57 seconds</p>
                 <div className="flex flex-wrap gap-x-5 gap-y-6 justify-center">
-                  <ResultCard imageResult="modalBackgroundType4.png" similarity="10"></ResultCard>
-                  <ResultCard imageResult="modalBackgroundType4.png" similarity="20"></ResultCard>
-                  <ResultCard imageResult="modalBackgroundType4.png" similarity="30"></ResultCard>
-                  <ResultCard imageResult="modalBackgroundType4.png" similarity="40"></ResultCard>
+                  <ResultCard
+                    imageResult="modalBackgroundType4.png"
+                    similarity="10"
+                  ></ResultCard>
+                  <ResultCard
+                    imageResult="modalBackgroundType4.png"
+                    similarity="20"
+                  ></ResultCard>
+                  <ResultCard
+                    imageResult="modalBackgroundType4.png"
+                    similarity="30"
+                  ></ResultCard>
+                  <ResultCard
+                    imageResult="modalBackgroundType4.png"
+                    similarity="40"
+                  ></ResultCard>
                 </div>
                 {/* pagination */}
-                <div className="mt-5">
-                  ________________
-                </div>
+                <div className="mt-5">________________</div>
               </div>
             </div>
           </div>
