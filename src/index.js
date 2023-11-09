bdb9f94fe22400b834bdc66f87aa0d4fc6c2eb3d
ChaiGans <elbertchailes888@gmail.com>
2023-11-10 03:23:52 +0700

    chore: render app

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './pages/App';
+import Form from './components/Form'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App />
-);
+root.render(<App />);
