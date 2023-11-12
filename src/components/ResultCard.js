f5cefae9a7d645c3d066cf85b6e8c74c072c185b
ChaiGans <elbertchailes888@gmail.com>
2023-11-13 01:17:06 +0700

    feat: props to handle image format

--- src/components/ResultCard.js
+++ src/components/ResultCard.js
@@ -1,10 +1,16 @@
 const ResultCard = (props) => {
     return (
       <div className="border-white w-[190px] border-2 flex flex-col">
-        <div className="border-white w-full order-2 h-[120px] border-b-2"> 
-        <img src={props.imageResult} className="w-full h-full object-cover object-center" alt="result"></img>
+        <div className="border-white w-full order-2 h-[120px] border-b-2">
+          <img
+            src={`data:image/${props.imageFormat};base64,${props.imageResult}`}
+            className="w-full h-full object-cover object-center"
+            alt="result"
+          ></img>
         </div>
-        <div className="border-white bg-black bg-opacity-60 w-full order-2 h-[30px] flex justify-center items-center font-aenoniklight text-[12px]">Similarity : {props.similarity}%</div>
+        <div className="border-white bg-black bg-opacity-60 w-full order-2 h-[30px] flex justify-center items-center font-aenoniklight text-[12px]">
+          Similarity : {props.similarity}%
+        </div>
       </div>
     );
 }
