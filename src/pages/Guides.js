075758815959c821175e8ee5b0df49ba8a7c708d
Bodleh <tfprince079@gmail.com>
2023-11-13 23:19:53 +0700

    feat : add Guides

---
+++ src/pages/Guides.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Guides() {
+    return (
+        <div className="min-w-screen min-h-screen flex flex-col">
+            <main className="flex-1 relative">
+            <img
+                src="backgroundCamera.jpg"
+                alt="Background"
+                className="fixed inset-0 object-cover w-full h-full "
+            />
+            <div className="bg-black bg-opacity-60 absolute inset-0" />
+            </main>
+        </div>
+    );
+}
+
+export default Guides;
