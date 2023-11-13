5730df4bdffaf7791a78076948c5471947a9fc21
ChaiGans <elbertchailes888@gmail.com>
2023-11-14 01:42:28 +0700

    fix: error on printing image url

--- src/program/imagescraping.go
+++ src/program/imagescraping.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"fmt"
-	"image"
 	"strings"
 
 	"net/http"
 	"net/url"
 
-	"github.com/FarelW/Algeo02-22045/src/program"
 	"github.com/gocolly/colly"
 )
 
@@ -26,13 +24,11 @@ func scrappingToUrlArray(urlStr string) []string {
 	var imageUrls []string
 
 	c.OnHTML("img[src]", func(e *colly.HTMLElement) {
-		// Extract the src attribute value
-		link := e.Attr("src")
-
-		// Check if the link ends with .jpg, .png, or .jpeg
+		link := e.Request.AbsoluteURL(e.Attr("src"))
 		if strings.HasSuffix(link, ".jpg") || strings.HasSuffix(link, ".png") || strings.HasSuffix(link, ".jpeg") {
 			imageUrls = append(imageUrls, link)
 		}
+		fmt.Println(link)
 	})
 
 	err = c.Visit(urlStr)
@@ -45,11 +41,12 @@ func scrappingToUrlArray(urlStr string) []string {
 }
 
 func main() {
-	defer program.timer("main")()  
-	urlToScrape := "https://scrapeme.live/shop/"
+	// defer program.timer("main")()
+	urlToScrape := "https://tokopedia.com/"
 	imageUrls := scrappingToUrlArray(urlToScrape)
 
-	fmt.Println("Image URLs:")
+	// imageUrls is array of image links
+	// fmt.Println("Image URLs:")
 	for _, imageUrl := range imageUrls {
 		resp, err := http.Get(imageUrl)
 		if err != nil {
@@ -62,30 +59,5 @@ func main() {
 			fmt.Println("Error: Non-OK status code received:", resp.Status)
 			return
 		}
-
-		img, _, err := image.Decode(resp.Body)
-		if err != nil {
-			fmt.Println("Error decoding image:", err)
-			return
-		}
-		imageFile1 := "../../img/beda1.png"
-		img2 := img
-		img1, err := program.loadImage(imageFile1)
-		if err != nil {
-			fmt.Println("Error loading the image:", err)
-			return
-		}
-
-		imgRGB1 := program.getNormalizedRGBValues(img1)
-		imgRGB2 := program.getNormalizedRGBValues(img2)
-		var imgHSV1 program.ImageHSV
-		var imgHSV2 program.ImageHSV
-		var vector1 [9][72]float32
-		var vector2 [9][72]float32
-		program.convertRGBToHSVValues(imgRGB1, &imgHSV1)
-		program.convertRGBToHSVValues(imgRGB2, &imgHSV2)
-		program.divideHSVMatrixTo9Vectors(imgHSV1, &vector1)
-		program.divideHSVMatrixTo9Vectors(imgHSV2, &vector2)
-		fmt.Printf("%.2f%%\n", 100*program.ArrayOfVectorCosineWeighting(vector1, vector2))
 	}
 }
