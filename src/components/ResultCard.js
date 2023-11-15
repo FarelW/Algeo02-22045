4dc5f4940dc926885d3d1be6cdc1430834506dcf
Farel Winalda <eeksapi321@gmail.com>
2023-11-16 03:51:11 +0700

    fix : result card props

--- src/components/ResultCard.js
+++ src/components/ResultCard.js
@@ -1,14 +1,15 @@
 const ResultCard = (props) => {
+  // console.log(props)
     return (
-      <div className="border-white w-[190px] border-2 flex flex-col">
-        <div className="border-white w-full order-2 h-[120px] border-b-2">
+      <div className="border-white w-[150px] border-2 flex flex-col">
+        <div className="border-white w-full order-2 h-[100px] border-b-2">
           <img
             src={`data:image/${props.imageFormat};base64,${props.imageResult}`}
             className="w-full h-full object-cover object-center"
             alt="result"
           ></img>
         </div>
-        <div className="border-white bg-black bg-opacity-60 w-full order-2 h-[30px] flex justify-center items-center font-aenoniklight text-[12px]">
+        <div className="border-white bg-black bg-opacity-60 w-full order-2 h-[25px] flex justify-center items-center font-aenoniklight text-[12px]">
           Similarity : {props.similarity}%
         </div>
       </div>
