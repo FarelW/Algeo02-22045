0fe9940714e901cfb9490a1b0a9f77cb443c21ba
Farel Winalda <eeksapi321@gmail.com>
2023-11-16 03:52:45 +0700

    feat : add endpoint for imagescraping and fixing new return

--- src/api/main.go
+++ src/api/main.go
@@ -5,10 +5,16 @@ import (
 	"encoding/base64"
 	"image"
 	"log"
+	"net/http"
+	"path/filepath"
+	"strings"
+	"io"
+	color "github.com/FarelW/Algeo02-22045/program"
+	texture "github.com/FarelW/Algeo02-22045/program"
+	imagescraping "github.com/FarelW/Algeo02-22045/program"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
-	texture "github.com/FarelW/Algeo02-22045/program"
-	color "github.com/FarelW/Algeo02-22045/program"
+	// "golang.org/x/tools/go/analysis/passes/appends"
 )
 
 
@@ -66,7 +72,7 @@ func main ()  {
 	
 		buf := new(bytes.Buffer)
 		buf.ReadFrom(imageFile)
-		// var temp string= base64.StdEncoding.EncodeToString(buf.Bytes())
+		var temp string= base64.StdEncoding.EncodeToString(buf.Bytes())
 
 		img, _, err := image.Decode(buf)
 		if err != nil {
@@ -75,24 +81,20 @@ func main ()  {
 		}
 
 		response := fiber.Map{}
+		var dataArray []fiber.Map
 		var textureVector texture.VectorCHE
 		var colorVector [9][72] float32
 
 		if isColor {
 			colorVector = color.ColorProcessing(img)
-			// response = fiber.Map{
-			// 	"imageFile": temp,
-			// 	"textureVector": colorVector,
-			// }
+			response["imageFile"] = fiber.Map{
+				"base64": temp,
+			}
 		} else {
 			textureVector = texture.TextureProcessing(img)
-			// log.Println("vector:",textureVector)
-			// log.Println("ini\n",base64.StdEncoding.EncodeToString(buf.Bytes()))
-
-			// response = fiber.Map{
-			// 	"imageFile": temp,
-			// 	"textureVector": textureVector,
-			// }
+			response["imageFile"] = fiber.Map{
+				"base64": temp,
+			}
 		}
 
 		// log.Printf("%s\n",response["imageFile"])
@@ -112,6 +114,17 @@ func main ()  {
 				log.Printf("Error opening file: %v\n", err)
 			}
 			defer file.Close()
+
+			extension := filepath.Ext(fileHeader.Filename)
+			extension = strings.ToLower(extension)
+
+			// Check if the extension is valid (jpg, jpeg, or png)
+			if extension != ".jpg" && extension != ".jpeg" && extension != ".png" {
+				log.Printf("Unsupported file format: %s\n", extension)
+				continue // Skip this file and proceed with the next one
+			}
+
+			var nameperfile string = fileHeader.Filename
 
 			buf := new(bytes.Buffer)
 			buf.ReadFrom(file)
@@ -134,129 +147,152 @@ func main ()  {
 			}
 
 			if sim >= 60 {
+
 				fileData := fiber.Map{
+					"fileName":nameperfile,
 					"base64": temp,
 					"Similarity": sim,
 				}
-				response[fileHeader.Filename] = fileData
+				dataArray = append(dataArray, fileData)
 			}
 		}
+
+		response["data"] = dataArray
 	
 		log.Println("Sending response")
 		return c.JSON(response)
 	})
 	
-	// app.Post("/api/scraping", func(c *fiber.Ctx) error {
-	// 	proccesstype := c.FormValue("proccesstype")
-	// 	if proccesstype == "" {
-	// 		log.Printf("Error detecting process type: not provided\n")
-	// 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Process type not found"})
-	// 	}
-
-	// 	var isColor bool
-	// 	if proccesstype == "true" {
-	// 		isColor = true
-	// 	} else if proccesstype == "false" {
-	// 		isColor = false
-	// 	} else {
-	// 		log.Printf("Error: Invalid process type value\n")
-	// 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid process type"})
-	// 	}
-
-	// 	imageFileHeader, err := c.FormFile("imageFile")
-	// 	if err != nil {
-	// 		log.Printf("Error retrieving the image file: %v\n", err)
-	// 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "image file not found"})
-	// 	}
+	app.Post("/api/scrape", func(c *fiber.Ctx) error {
+		log.Println("Scrape endpoint hit")
 	
-	// 	imageFile, err := imageFileHeader.Open()
-	// 	if err != nil {
-	// 		log.Printf("Error opening the image file: %v\n", err)
-	// 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "error opening image file"})
-	// 	}
-	// 	defer imageFile.Close()
+		// Retrieve process type
+		proccesstype := c.FormValue("proccesstype")
+		if proccesstype == "" {
+			log.Printf("Error detecting process type: not provided\n")
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Process type not found"})
+		}
 	
-	// 	buf := new(bytes.Buffer)
-	// 	buf.ReadFrom(imageFile)
-	// 	// var temp string= base64.StdEncoding.EncodeToString(buf.Bytes())
-
-	// 	img, _, err := image.Decode(buf)
-	// 	if err != nil {
-	// 		log.Printf("Error decoding the image: %v\n", err)
-	// 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "error decoding image"})
-	// 	}
-
-	// 	response := fiber.Map{}
-	// 	var textureVector texture.VectorCHE
-	// 	var colorVector [9][72] float32
-
-	// 	if isColor {
-	// 		colorVector = color.ColorProcessing(img)
-	// 		// response = fiber.Map{
-	// 		// 	"imageFile": temp,
-	// 		// 	"textureVector": colorVector,
-	// 		// }
-	// 	} else {
-	// 		textureVector = texture.TextureProcessing(img)
-	// 		// log.Println("vector:",textureVector)
-	// 		// log.Println("ini\n",base64.StdEncoding.EncodeToString(buf.Bytes()))
-
-	// 		// response = fiber.Map{
-	// 		// 	"imageFile": temp,
-	// 		// 	"textureVector": textureVector,
-	// 		// }
-	// 	}
-
-	// 	// log.Printf("%s\n",response["imageFile"])
-	// 	log.Println("Image file processed")
+		var isColor bool
+		if proccesstype == "true" {
+			isColor = true
+		} else if proccesstype == "false" {
+			isColor = false
+		} else {
+			log.Printf("Error: Invalid process type value\n")
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid process type"})
+		}
 	
-	// 	// Handle multiple dataset files upload
-	// 	form, err := c.MultipartForm()
-	// 	if err != nil {
-	// 		log.Printf("Error retrieving multipart form: %v\n", err)
-	// 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "error retrieving files"})
-	// 	}
-
-	// 	// files := form.File["UrlScraping"]
-	// 	// for _, fileHeader := range files {
-	// 	// 	file, err := fileHeader.Open()
-	// 	// 	if err != nil {
-	// 	// 		log.Printf("Error opening file: %v\n", err)
-	// 	// 	}
-	// 	// 	defer file.Close()
-
-	// 	// 	buf := new(bytes.Buffer)
-	// 	// 	buf.ReadFrom(file)
-	// 	// 	// var halo string=fileHeader.Filename
-	// 	// 	var temp string= base64.StdEncoding.EncodeToString(buf.Bytes())
-
-	// 	// 	img, _, err := image.Decode(buf)
-	// 	// 	if err != nil {
-	// 	// 		log.Printf("Error decoding the image: %v\n", err)
-	// 	// 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "error decoding image"})
-	// 	// 	}
-
-	// 	// 	var sim float32
-	// 	// 	if isColor {
-	// 	// 		var compareData [9][72]float32 = color.ColorProcessing(img)
-	// 	// 		sim = color.ArrayOfVectorCosineWeighting(colorVector, compareData)
-	// 	// 	} else {
-	// 	// 		var compareData texture.VectorCHE = texture.TextureProcessing(img)
-	// 	// 		sim = texture.TextureSimilarity(textureVector, compareData)
-	// 	// 	}
-
-	// 	// 	if sim >= 60 {
-	// 	// 		fileData := fiber.Map{
-	// 	// 			"base64": temp,
-	// 	// 			"Similarity": sim,
-	// 	// 		}
-	// 	// 		response[fileHeader.Filename] = fileData
-	// 	// 	}
-	// 	// }
+		// Retrieve the image file
+		imageFileHeader, err := c.FormFile("imageFile")
+		if err != nil {
+			log.Printf("Error retrieving the image file: %v\n", err)
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "image file not found"})
+		}
 	
-	// 	log.Println("Sending response")
-	// 	return c.JSON(response)
-	// })
+		imageFile, err := imageFileHeader.Open()
+		if err != nil {
+			log.Printf("Error opening the image file: %v\n", err)
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "error opening image file"})
+		}
+		defer imageFile.Close()
+	
+		buf := new(bytes.Buffer)
+		buf.ReadFrom(imageFile)
+		var temp string = base64.StdEncoding.EncodeToString(buf.Bytes())
+	
+		img, _, err := image.Decode(buf)
+		if err != nil {
+			log.Printf("Error decoding the image: %v\n", err)
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "error decoding image"})
+		}
+
+		response := fiber.Map{}
+		var dataArray []fiber.Map
+		var textureVector texture.VectorCHE
+		var colorVector [9][72]float32
+
+		if isColor {
+			colorVector = color.ColorProcessing(img)
+			response["imageFile"] = fiber.Map{
+				"base64": temp,
+			}
+			// log.Println("color:",colorVector)
+		} else {
+			textureVector = texture.TextureProcessing(img)
+			response["imageFile"] = fiber.Map{
+				"base64": temp,
+			}
+			log.Println("texture:",textureVector)
+		}
+
+		var scrapUrl string= c.FormValue("Scrapurl")
+		if scrapUrl == "" {
+			log.Printf("Scrapurl not provided\n")
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Scrapurl not found"})
+		}
+
+		imageUrls := imagescraping.ScrappingToUrlArray(scrapUrl)
+		
+		for _, imageUrl := range imageUrls {
+			resp, err := http.Get(imageUrl)
+			if err != nil {
+				log.Println("Error downloading image:", err)
+				continue // skip to next image
+			}
+		
+			if !strings.HasPrefix(resp.Header.Get("Content-Type"), "image/") {
+				log.Println("URL is not an image:", imageUrl)
+				resp.Body.Close()
+				continue
+			}
+		
+			if resp.StatusCode != http.StatusOK {
+				log.Println("Error: Non-OK status code received:", resp.Status)
+				resp.Body.Close()
+				continue
+			}
+		
+			imgData, err := io.ReadAll(resp.Body)
+			resp.Body.Close() // Close the response body immediately after reading
+			if err != nil {
+				log.Println("Error reading image data:", err)
+				continue
+			}
+		
+			imgReader := bytes.NewReader(imgData)
+			decodedImg, _, err := image.Decode(imgReader)
+			if err != nil {
+				log.Println("Error decoding image:", err)
+				continue
+			}
+		
+			var sim float32
+			if isColor {
+				compareData := color.ColorProcessing(decodedImg)
+				sim = color.ArrayOfVectorCosineWeighting(colorVector, compareData)
+			} else {
+				compareData := texture.TextureProcessing(decodedImg)
+				sim = texture.TextureSimilarity(textureVector, compareData)
+			}
+		
+			temp := base64.StdEncoding.EncodeToString(imgData)
+			if sim >= 60 {
+				fileData := fiber.Map{
+					"fileName":   imageUrl,
+					"base64":     temp,
+					"Similarity": sim,
+				}
+				dataArray = append(dataArray, fileData)
+			}
+		}
+		response["data"] = dataArray
+
+		log.Println("Sending response")
+		return c.JSON(response)
+		
+	})
+
 
 	app.Get("/api/upload", func(c *fiber.Ctx) error {
 		data := map[string]interface{}{
@@ -266,23 +302,13 @@ func main ()  {
 		return c.JSON(data)
 	})
 
-	app.Post("/api/webcam/snapshot", func(c *fiber.Ctx) error {
-		type ImageData struct {
-			Image string `json:"image"`
+	app.Get("/api/scraping", func(c *fiber.Ctx) error {
+		data := map[string]interface{}{
+			"message": "GET request to /api/upload handled successfully",
 		}
 	
-		var imageData ImageData
-		if err := c.BodyParser(&imageData); err != nil {
-			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "request body parsing error"})
-		}
-	
-		// Decode the base64 image
-		// Process or store the image as needed
-		// ...
-	
-		return c.Status(fiber.StatusOK).JSON(fiber.Map{"message": "Image received"})
+		return c.JSON(data)
 	})
-	
 
 	if err := app.Listen(":8080"); err != nil {
 		log.Fatal(err)
